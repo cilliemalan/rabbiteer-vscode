530d2e5060f249148f1f5bba3472beb53a21547e
Cillié Malan <cilliemalan@users.noreply.github.com>
2016-09-27 10:12:29 +0200

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,14 +2,12 @@
 let a = 10;
 let b = 20;
 
-
 console.log('hello world');
 
 //print some stuffs
 printstuff((b - a) / 2);
 
 function printstuff(cnt) {
-
     for (let i = 0; i < cnt; i++) {
         console.log(`hello world ${i}`);
     }
